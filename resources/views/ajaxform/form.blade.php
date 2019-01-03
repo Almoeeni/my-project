d2cbe32a8e31aefc8b615ec7842509a7cc239ba8
Almoeeni <Almoiniefazal@live.co.uk>
2019-01-03 23:06:49 +0500

    validation dynamic

--- resources/views/ajaxform/form.blade.php
+++ resources/views/ajaxform/form.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laravel Ajax Validation Example</title>
+	<title>Ajax Validation Example</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 	<meta name="_token" content="{{csrf_token()}}" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 </head>
+
+<style>
+.bcolor {
+	border: 1px solid green;
+}
+</style>
 <body>
 
 
 <div class="container">
-	<h2>Laravel Ajax Validation</h2>
+	<h2>Ajax Validation</h2>
 
 	<!-- <div class="alert alert-danger" style="display:none">
 	<p>success</p>
@@ -21,40 +27,47 @@
 
 <div id="done">
 
+
 </div>
+
+
 
 	<form action="/ajaxform" method="post">
 		{{ csrf_field() }}
-		<div class="form-group">
-			<label> Name:</label>
 
-			<input type="text" id="name" name="name" class="form-control">
-			<span id="nam"></span>
-			
+
+		
+
+
+		<div class="form-group start">
+			<label class="control-label" for="inputSuccess1">Name:</label>
+			<input type="text" id="name" name="name" class="form-control" id="inputSuccess1" aria-describedby="helpBlock2">		
+		</div>
+
+	
+		<div class="form-group start">
+			<label class="control-label" for="inputSuccess1">Email:</label>
+			<input type="email" id="email" name="email" class="form-control" id="inputSuccess1" aria-describedby="helpBlock2">
 		</div>
 
 
-		<div class="form-group">
-			<strong>Email:</strong>
-			<input type="text" id="email" name="email" class="form-control" >
-			<span id="eml"></span>
+		<div class="form-group start">
+			<label class="control-label" for="inputSuccess1">Password:</label>
+            <input type="password" id="password" name="password" class="form-control" id="inputSuccess1" aria-describedby="helpBlock2">			
 		</div>
 
-
-		<div class="form-group">
-			<strong>Password:</strong>
-            <input type="password" id="password" name="password" class="form-control">
-			<span id="pass"></span>
-		</div>
-        <div class="form-group">
-			<strong> Confirm Password:</strong>
-            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+        <div class="form-group start">
+			<label class="control-label" for="inputSuccess1">Confirm Password:</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"id="inputSuccess1" aria-describedby="helpBlock2">		
 		</div>
 
 
 		<div class="form-group">
 			<button class="btn btn-success btn-submit">Submit</button>
 		</div>
+
+
+	
 	</form>
 </div>
 
@@ -63,8 +76,27 @@
 
 
 	$(document).ready(function() {
+		
+		$("#email").on('keypress',function()  {
+    if ($(this).val() != null) {
+      
+        $("input[type=email]").css('border','1px solid green');
+    } else  {
+
+        $("input[type=email]").css('border','1px solid white');
+    }
+});
+		//$('.help-block').removeClass('hide');
+		// $('input').keypress(function(){
+		// 	$('input').css("border-color" ,"green");
+		// })
+	
 	    $(".btn-submit").click(function(e){
+			
+			//$('.help-block').addClass('hide');
+			$('.help-block').show();
 	    	e.preventDefault();
+			resetErrors();
 			$.ajaxSetup({
                   headers: {
                       'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
@@ -75,43 +107,35 @@
 	        $.ajax({
 	            url: "/ajaxform",
 	            type:'POST',
+				
 				data: {
                      name: $('#name').val(),
                      email: $('#email').val(),
                      password: $('#password').val(),
-					 password_confirmation: $('#password_confirmation').val()
-					
+					 password_confirmation: $('#password_confirmation').val()					
                   },
+				  
 				  success: function(data){
 					if($.isEmptyObject(data.error)){
 						$("#done").html(data.success);
 						$(".print-error-msg").hide();
+					
+					
 
 	                }else{
-						//console.log(data.error[0]);
-	                	 //printErrorMsg(data.error);
-					//	$("#done").html(data.error);
-				//	$("#done").html(data.error[0]);
 					
-					// $("#nam").html(data.error[0];
-					// if(data.error[0] ){
-					// 	$("#nam").html(data.error[0]);
-					// }
-					// if(data.error[1]){
-					// 	$("#eml").html(data.error[1]);
-					// }
-					// if(data.error[2]){
-					// 	$("#pass").html(data.error[2]);
-					// }
 					
-					// $("#eml").html(data.error[1]);
-					// $("#pass").html(data.error[2]);
-
-					$(".print-error-msg").find("ul").html('');
-					$(".print-error-msg").css('display','block');
-					$.each( data.error, function( key, value ) {
-					$(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+					$.each( data.error, function( i, v ) {
+						console.log(i + " => " + v);
+						
+						var err = '<span id="helpBlock2" class="help-block" for="'+i+'">'+v+'</span>';  										
+                        $('input[name="' + i + '"]').after(err).parent('div.start').addClass('has-error');
+						
 					});
+
+					
+				//print(data.error);
+
 
 	               }
 				
@@ -122,8 +146,26 @@
 
 
 	    });
+
+		function print(msg){
+			$(".print-error-msg").find("ul").html('');
+					$(".print-error-msg").css('display','block');
+					$.each( msg, function( key, value ) {
+					$(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+					});
+
+		}
+
+		function resetErrors() 
+		{
+   			 $('div.start').removeClass('has-error');
+			
+   			 $('span.help-block').remove();
+				
+		}
 	
 	});
+
 
 
 
